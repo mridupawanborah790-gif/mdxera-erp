cd4e07a3e5276e121a483c8825d8c113eca304f9
mridupawanborah790-gif <mridupawanborah790@gmail.com>
2026-02-24 16:50:15 +0530

    Add tier-rate prompt and next-product flow in purchase entry

--- constants.tsx
+++ constants.tsx
@@ -354,6 +354,8 @@ export const configurableModules = [
       { id: 'colBrand', name: 'MFR / Brand' },
       { id: 'colBatch', name: 'Batch' },
       { id: 'colExpiry', name: 'Exp.' },
+      { id: 'colPack', name: 'Pack' },
+      { id: 'colFree', name: 'FREE' },
       { id: 'colMrp', name: 'MRP' },
       { id: 'colQty', name: 'Qty' },
       { id: 'colPurRate', name: 'Pur. Rate' },
